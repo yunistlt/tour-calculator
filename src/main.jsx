6cfe58086302c657dbdb19b8a82810abf9a90d22
yunistlt <yunistgl@gmail.com>
2025-09-20 16:09:47 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './ui/base.css'
+
 import App from './ui/App.jsx'
 import Login from './ui/Login.jsx'
 import AdminLogin from './ui/AdminLogin.jsx'
-import AdminPanel from './ui/AdminPanel.jsx'
-import AdminUsers from './ui/AdminUsers.jsx'
-import './ui/base.css'
+import AdminServices from './ui/AdminServices.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/admin/login" element={<AdminLogin/>} />
-        <Route path="/admin" element={<AdminPanel/>} />
-        <Route path="/admin/users" element={<AdminUsers/>} />
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+
+        {/* только одна админ-страница */}
+        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/admin" element={<AdminServices />} />
+
+        <Route path="*" element={
+          <div style={{padding:24}}>
+            <h2>Страница не найдена</h2>
+            <p><a href="/">На главную</a></p>
+          </div>
+        } />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
