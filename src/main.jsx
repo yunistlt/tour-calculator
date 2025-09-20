452b3a2534859c65c1c5d441d81c1fcf46bd1741
yunistlt <yunistgl@gmail.com>
2025-09-20 17:27:42 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,9 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './ui/base.css'
 
-// основные страницы
 import App from './ui/App.jsx'
 import Login from './ui/Login.jsx'
 import Register from './ui/Register.jsx'
-
-// админка
 import AdminLogin from './ui/AdminLogin.jsx'
 import AdminServices from './ui/AdminServices.jsx'
 
@@ -17,16 +14,16 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        {/* Фронт-энд (пользовательская часть) */}
+        {/* Пользовательская часть */}
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} /> {/* ✅ маршрут регистрации */}
+        <Route path="/register" element={<Register />} />
 
         {/* Админка */}
         <Route path="/admin/login" element={<AdminLogin />} />
         <Route path="/admin" element={<AdminServices />} />
 
-        {/* 404 — fallback */}
+        {/* 404 */}
         <Route
           path="*"
           element={
